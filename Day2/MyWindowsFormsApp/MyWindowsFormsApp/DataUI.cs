3694bdd5619eaece476cf01a8ed4a951d31d17cc
shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-09-12 12:06:36 +0600

    newForm

---
+++ Day2/MyWindowsFormsApp/MyWindowsFormsApp/DataUI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWindowsFormsApp
+{
+    public partial class DataUI : Form
+    {
+        public DataUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            /*int age = 0;
+            double salary = 5000.50;
+            String name = "Hasan";
+            bool isDone = true;*/
+
+            String name = nameTextBox.Text;
+
+            int firstNumber = int.Parse(firstNumberTextBox.Text);
+            int secondNumber = int.Parse(secondNumberTextBox.Text);
+
+            int result = firstNumber + secondNumber;
+
+            //MessageBox.Show("Name: " + name + " result = " + result.ToString());
+            MessageBox.Show(result + " ");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Day2/MyWindowsFormsApp/MyWindowsFormsApp/Program.cs
+++ Day2/MyWindowsFormsApp/MyWindowsFormsApp/Program.cs
@@ -16,7 +16,8 @@ namespace MyWindowsFormsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            //Application.Run(new Home());
+            Application.Run(new DataUI());
         }
     }
 }
