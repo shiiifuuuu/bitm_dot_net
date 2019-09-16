42d89b837157c0f6c136f14ff8fab46d7b98c7df
Shahadat <mdshahadat.hossain7@gmail.com>
2019-09-16 22:46:37 +0600

    day1 day3

---
+++ Day1/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, age, address, contact;
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            age = Console.ReadLine();
+            Console.Write("Please enter your address: ");
+            address = Console.ReadLine();
+            Console.Write("Please enter your contact: ");
+            contact = Console.ReadLine();
+
+            Console.WriteLine("\nOutput: ");
+            Console.WriteLine("Form Information: \n");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Contact: " + contact);
+
+            Console.Write("\nPress any key to return...");
+            Console.ReadKey();
+        }
+    }
+}
