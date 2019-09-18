a111ce8cd4704abee613bfb2111027e0c4d35b33
Shahadat <mdshahadat.hossain7@gmail.com>
2019-09-18 23:35:46 +0600

    exercise

---
+++ _Exercise/ArrayExercise/ArrayExercise/ArrayExercise.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ArrayExercise
+{
+    partial class ArrayExercise
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showElementsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.addSizeButton = new System.Windows.Forms.Button();
+            this.arraySizeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.sumButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.addButton);
+            this.groupBox1.Controls.Add(this.addTextBox);
+            this.groupBox1.Controls.Add(this.showButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 160);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Exercise 1";
+            // 
+            // showElementsRichTextBox
+            // 
+            this.showElementsRichTextBox.Location = new System.Drawing.Point(11, 19);
+            this.showElementsRichTextBox.Name = "showElementsRichTextBox";
+            this.showElementsRichTextBox.Size = new System.Drawing.Size(276, 157);
+            this.showElementsRichTextBox.TabIndex = 4;
+            this.showElementsRichTextBox.Text = "";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(113, 83);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Location = new System.Drawing.Point(10, 47);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(178, 20);
+            this.addTextBox.TabIndex = 2;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(10, 131);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(178, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show Elements";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Store elements in an array and print it";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.sizeLabel);
+            this.groupBox2.Controls.Add(this.addSizeButton);
+            this.groupBox2.Controls.Add(this.arraySizeTextBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 178);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 67);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Exercise 2";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(135, 47);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(0, 13);
+            this.sizeLabel.TabIndex = 3;
+            // 
+            // addSizeButton
+            // 
+            this.addSizeButton.Location = new System.Drawing.Point(78, 37);
+            this.addSizeButton.Name = "addSizeButton";
+            this.addSizeButton.Size = new System.Drawing.Size(57, 23);
+            this.addSizeButton.TabIndex = 2;
+            this.addSizeButton.Text = "Add";
+            this.addSizeButton.UseVisualStyleBackColor = true;
+            this.addSizeButton.Click += new System.EventHandler(this.addSizeButton_Click);
+            // 
+            // arraySizeTextBox
+            // 
+            this.arraySizeTextBox.Location = new System.Drawing.Point(6, 40);
+            this.arraySizeTextBox.Name = "arraySizeTextBox";
+            this.arraySizeTextBox.Size = new System.Drawing.Size(65, 20);
+            this.arraySizeTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Input the number of elements to store in the array:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sumButton);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 251);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(160, 77);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Exercise 3";
+            // 
+            // sumButton
+            // 
+            this.sumButton.Location = new System.Drawing.Point(35, 46);
+            this.sumButton.Name = "sumButton";
+            this.sumButton.Size = new System.Drawing.Size(75, 23);
+            this.sumButton.TabIndex = 2;
+            this.sumButton.Text = "Show Sum";
+            this.sumButton.UseVisualStyleBackColor = true;
+            this.sumButton.Click += new System.EventHandler(this.sumButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sum of all elements in the array";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.copyButton);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Location = new System.Drawing.Point(12, 334);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(165, 77);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Exercise 4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Copy one array to another array";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.showElementsRichTextBox);
+            this.groupBox5.Location = new System.Drawing.Point(183, 251);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(293, 160);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Result";
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(46, 48);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 1;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // ArrayExercise
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 423);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ArrayExercise";
+            this.Text = "Array Exercise";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button addSizeButton;
+        private System.Windows.Forms.TextBox arraySizeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button sumButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox showElementsRichTextBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button copyButton;
+    }
+}
+
---
+++ _Exercise/ArrayExercise/ArrayExercise/ArrayExercise.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayExercise
+{
+    public partial class ArrayExercise : Form
+    {
+
+        const int size = 2;
+        int userSize = 0;
+        int count = 0;
+        int length;
+        String[] elements = new String[size];
+
+        public ArrayExercise()
+        {
+            InitializeComponent();
+            length = elements.Length;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if(userSize != 0)
+            {
+                addElements(userSize);
+            }
+            else
+            {
+                addElements(size);
+            }
+        }
+
+        private void addElements(int size)
+        {
+            if (count != size)
+            {
+                elements[count] = addTextBox.Text;
+                addTextBox.Text = "";
+                count++;
+                if((length-1) == 0)
+                {
+                    MessageBox.Show("No more elements to add");
+                }
+                else
+                {
+                    MessageBox.Show("Add " + (length - 1) + " more elements");
+                }
+                length--;
+            }
+            else
+            {
+                MessageBox.Show("Size Exceed!!! Input your size to show more elements.");
+                addTextBox.Text = "";
+            }
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            showElementsRichTextBox.Text = "Elements in array are: " + showElementsFromArray(elements);
+        }
+
+        private String showElementsFromArray(String[] array)
+        {
+            String message = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                message += array[i] + " ";
+            }
+            return message;
+        }
+
+        private void addSizeButton_Click(object sender, EventArgs e)
+        {
+            userSize = int.Parse(arraySizeTextBox.Text);
+            arraySizeTextBox.Text = "";
+            elements = new String[userSize];
+            length = elements.Length;
+            count = 0;
+            sizeLabel.Text = "Input " + userSize + " elements in the array.";
+        }
+
+
+        private void sumButton_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int i=0; i<elements.Length; i++)
+            {
+                sum += int.Parse(elements[i]);
+            }
+            showElementsRichTextBox.Text = "Sum of all elemetns stored in array: " + sum;
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            String[] newArray = new String[elements.Length];
+            elements.CopyTo(newArray, 0);
+            String array1 = showElementsFromArray(elements);
+            String array2 = showElementsFromArray(newArray);
+
+            showElementsRichTextBox.Text = "The elements stored in first Array: " + array1 
+                                            + "\n\n" 
+                                            + "The elements stored in second Array: " + array2;
+        }
+    }
+}
