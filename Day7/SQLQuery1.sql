2d500d41a41f5a0ccb84763d7f1b5d65e3f1bf63
Junnatul Ferdouse prome <junnatul.ferdouse@gmail.com>
2019-09-24 12:23:05 +0600

    sqlQuery

--- Day7/SQLQuery1.sql
+++ Day7/SQLQuery1.sql
@@ -43,3 +43,32 @@ SET Name = 'Cuppochino', Price = 160
 WHERE ID = 6
 
 SELECT name, price FROM items where  price >= 80 order by price desc
+
+
+CREATE TABLE customer
+(
+ID int IDENTITY(1,1),
+Name varchar(20),
+Contact varchar(11),
+Address varchar(30)
+)
+
+SELECT * FROM customer
+
+INSERT INTO customer (Name, Contact, Address)
+VALUES ('Shahadat', 01757814608, 'Dhaka')
+INSERT INTO customer VALUES ('Adnan', '01236547851', 'Shamoly')
+INSERT INTO customer VALUES ('Saykat', '32132543131', 'Kollanpur')
+INSERT INTO customer VALUES ('Raqib', '16541341645', 'Dhanmondi')
+INSERT INTO customer VALUES ('Shad', '017578456408', 'Mirpur')
+
+DROP TABLE customer
+
+INSERT INTO customer VALUES ('Adnan', '01236547851', 'Shamoly')
+INSERT INTO customer VALUES ('Saykat', '32132543131', 'Kollanpur')
+INSERT INTO customer VALUES ('Raqib', '16541341645', 'Dhanmondi')
+INSERT INTO customer VALUES ('Shad', '017578456408', 'Mirpur')
+
+UPDATE customer 
+SET Name = 'AAAA'
+WHERE ID = 5
