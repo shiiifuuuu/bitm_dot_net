db84928015c66f0a12c66f5ba7b6df2e719cd645
Junnatul Ferdouse prome <junnatul.ferdouse@gmail.com>
2019-09-24 11:29:31 +0600

    sqlQuery

--- Day7/SQLQuery1.sql
+++ Day7/SQLQuery1.sql
@@ -8,3 +8,37 @@ name	VARCHAR(50),
 price	FLOAT
 
 )
+
+SELECT * FROM item
+
+INSERT INTO item (ID, name, price) VALUES (1, 'Black', 120)
+INSERT INTO item VALUES (2, 'Cold', 100)
+INSERT INTO item VALUES (3, 'Hot', 90)
+
+DROP TABLE item
+
+CREATE TABLE items(
+ID	INT IDENTITY(1,1),
+name	VARCHAR(50),
+price	FLOAT
+)
+
+SELECT * FROM items
+
+INSERT INTO items VALUES ('Black', 120)
+INSERT INTO items VALUES ('Cold', 100)
+INSERT INTO items VALUES ('Hot', 90)
+
+UPDATE items 
+SET Name = 'Regular', Price = 80
+WHERE ID = 4
+
+UPDATE items 
+SET Name = 'Normal', Price = 60
+WHERE ID = 5
+
+UPDATE items 
+SET Name = 'Cuppochino', Price = 160
+WHERE ID = 6
+
+SELECT name, price FROM items where  price >= 80 order by price desc
