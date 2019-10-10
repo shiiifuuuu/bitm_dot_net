ed748f189d8ee622a88741ffa4e81e37e29bde15
Shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-10-10 12:49:07 +0600

    shahadat

---
+++ Day13/Introduction OOP/Practices/AccountOperationAppPractice1/AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    public class Account
+    {
+        public string accountNo;
+        public string name;
+        public double balance;
+
+        public string GetReport()
+        {
+            string report = name + ", your account number: " + accountNo + " and it's balance: " + balance + " taka";
+            return report;
+        }
+        public double Deposit(double balance)
+        {
+            double totalAmount=0;
+            totalAmount += balance;
+            return totalAmount;
+        }
+        public void Withdraw()
+        {
+
+        }
+    }
+}
---
+++ Day13/Introduction OOP/Walkthroughs/PersonInformationAppWalkthrough2/PersonInformationAppWalkthrough2/PersonInformationUi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonInformationAppWalkthrough2
+{
+    public partial class PersonInformationUi : Form
+    {
+        public PersonInformationUi()
+        {
+            InitializeComponent();
+        }
+
+
+        Person aPerson = new Person();
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            string fullName = aPerson.GetFullName();
+            fullNameTextBox.Text = fullName;
+
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            
+             aPerson.firstName = firstNameTextBox.Text;
+             aPerson.middleName = middleNameTextBox.Text;
+             aPerson.lastName = lastNameTextBox.Text;
+
+
+            firstNameTextBox.Clear();
+            middleNameTextBox.Clear();
+            lastNameTextBox.Clear();
+        }
+    }
+}
