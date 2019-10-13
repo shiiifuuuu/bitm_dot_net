d35937a87852cc998e607bb68051b8652ff2456f
Shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-10-13 10:19:36 +0600

    day14

---
+++ Day14/PersonInformationAppWalkthrough2/PersonInformationAppWalkthrough2/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInformationAppWalkthrough2
+{
+    class Person
+    {
+        public string FirstName { set; get; }
+        public string MiddleName { set; get; }
+        public string LastName { set; get; }
+
+        public string GetFullName()
+        {
+            string fullName = this.FirstName + " " + this.MiddleName + " " + this.LastName;
+            return fullName;
+        }
+    }
+}
---
+++ Day14/PersonInformationAppWalkthrough2/PersonInformationAppWalkthrough2/PersonInformationUi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonInformationAppWalkthrough2
+{
+    public partial class PersonInformationUi : Form
+    {
+        public PersonInformationUi()
+        {
+            InitializeComponent();
+        }
+
+
+        Person aPerson = new Person();
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            string fullName = aPerson.GetFullName();
+            fullNameTextBox.Text = fullName;
+
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            
+             aPerson.FirstName = firstNameTextBox.Text;
+             aPerson.MiddleName = middleNameTextBox.Text;
+             aPerson.LastName = lastNameTextBox.Text;
+
+            firstNameTextBox.Clear();
+            middleNameTextBox.Clear();
+            lastNameTextBox.Clear();
+        }
+    }
+}
