b148849009565868502a931f0c84a6434dfba0c9
Shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-10-15 00:03:13 +0600

    Refrigerator

---
+++ Day14/Assignment11/Assignment11/MODEL/Refrigerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11.MODEL
+{
+    public class Refrigerator
+    {
+        public double MaximumWeight { set; get; }
+
+        public Refrigerator(double maximumWeight)
+        {
+            this.MaximumWeight = maximumWeight;
+        }
+
+        public Refrigerator()
+        {
+        }
+
+        double currentWeight = 0;
+        public double CurrentWeight(double weight)
+        {
+            currentWeight+= weight;
+            return currentWeight;
+        }
+
+        public double RemainingWeight()
+        {
+            double remainingWeight = this.MaximumWeight - currentWeight;
+            return remainingWeight;
+        }
+    }
+}
---
+++ Day14/Assignment11/Assignment11/RefrigeratorForm.cs
@@ -0,0 +1,50 @@
+﻿using Assignment11.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment11
+{
+    public partial class RefrigeratorForm : Form
+    {
+        Refrigerator _refrigerator;
+
+        public RefrigeratorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            _refrigerator = new Refrigerator(double.Parse(maximumWeightTextBox.Text));
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            int itemNo = int.Parse(itemNoTextBox.Text);
+
+            if(double.Parse(itemWeightTextBox.Text) <= _refrigerator.MaximumWeight)
+            {
+                double currentWeight = _refrigerator.CurrentWeight(double.Parse(itemWeightTextBox.Text));
+
+
+                if (double.Parse(currentWeightTextBox.Text) <= _refrigerator.MaximumWeight)
+                {
+                    
+                    remainingWeightTextBox.Text = _refrigerator.RemainingWeight() + "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Weight Overflow!");
+            }
+            
+        }
+    }
+}
