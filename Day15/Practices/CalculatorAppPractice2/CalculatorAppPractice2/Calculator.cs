c1ef9716f3fb7ee0570befd34a28e68d6f60badd
Shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-10-15 23:29:26 +0600

    Practice2

---
+++ Day15/Practices/CalculatorAppPractice2/CalculatorAppPractice2/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppPractice2
+{
+    public class Calculator
+    {
+        public double FirstNumber { set; get; }
+        public double SecondNumber { set; get; }
+
+        public Calculator(double firstNumber, double secondNumber)
+        {
+            this.FirstNumber = firstNumber;
+            this.SecondNumber = secondNumber;
+        }
+
+        public double Add()
+        {
+            return (FirstNumber + SecondNumber);
+        }
+        public double Subtract()
+        {
+            return (FirstNumber - SecondNumber);
+        }
+        public double Multiply()
+        {
+            return (FirstNumber * SecondNumber);
+        }
+        public double Divide()
+        {
+            return (FirstNumber/SecondNumber);
+        }
+    }
+}
--- Day15/Practices/CalculatorAppPractice2/CalculatorAppPractice2/CalculatorUi.cs
+++ Day15/Practices/CalculatorAppPractice2/CalculatorAppPractice2/CalculatorUi.cs
@@ -12,9 +12,62 @@ namespace CalculatorAppPractice2
 {
     public partial class CalculatorUi : Form
     {
+        Calculator _calculator;
         public CalculatorUi()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _calculator = new Calculator(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text));
+                resultTextBox.Text = _calculator.Add() + "";
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void SubtractButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _calculator = new Calculator(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text));
+                resultTextBox.Text = _calculator.Subtract() + "";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _calculator = new Calculator(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text));
+                resultTextBox.Text = _calculator.Multiply() + "";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _calculator = new Calculator(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text));
+                resultTextBox.Text = _calculator.Divide() + "";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
     }
 }
